a6ef05561d6b3f8418824f359688f2fe0bf75c45
JoaoGripp <corguinha@gmail.com>
2021-10-28 09:37:31 -0300

    Challenge Day 18 Done

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,20 +11,29 @@ struct ContentView: View {
     
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
+    @State private var tipPercentage = 0.2
     @FocusState private var amountIsFocused: Bool
     
-    let tipPercentages = [10, 15, 20, 25, 0]
+    let tipPercentages = [0...100.0]
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
         
-        let tipValue = checkAmount / 100 * tipSelection
+        let tipValue = checkAmount * tipSelection
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
         
         return amountPerPerson
+    }
+    
+    var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
     }
     
     var body: some View {
@@ -36,25 +45,40 @@ struct ContentView: View {
                         .focused($amountIsFocused)
                     
                     Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
+                        ForEach(2..<101) {
                             Text("\($0) people")
                         }
                     }
                 }
                 Section {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
-                            Text($0, format: .percent)
-                        }
+                    HStack {
+                        Text(tipPercentage, format: .percent)
+                            .frame(width: 50)
+                        Spacer()
+                        
+                        Slider(value: $tipPercentage, in: 0...1, step: 0.01)
                     }
-                    .pickerStyle(.segmented)
+                    
+//                    Picker("Tip percentage", selection: $tipPercentage) {
+//                        ForEach(2..<100) {
+//                            Text($0, format: .percent)
+//                        }
+//                    }
+//                    .pickerStyle(.wheel)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
                 
+                Section {
+                    Text(totalAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Total Amount")
+                }
                 
                 Section {
                     Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                } header: {
+                    Text("Amount per person")
                 }
             }
             .navigationTitle("WeSplit")
